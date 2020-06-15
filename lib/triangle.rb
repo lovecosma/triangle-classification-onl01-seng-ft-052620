08a5ac5342fbb1737b04b7f75dd180bba3bb8e70
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-15 19:25:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,8 +21,9 @@ elsif @side_a == @side_c && @side_b != @side_a && @side_c != @side_b
   return :isosceles
 elsif @side_b == @side_c && @side_b != @side_a && @side_a != @side_c
   return :isosceles
-elsif @side_a == 0 && @side_b == 0 && @side_c == 0
   binding.pry
+elsif @side_a == 0 && @side_b == 0 && @side_c == 0
+
 elsif @side_a < 0 || @side_b < 0 || @side_c <  0
 end
 end
